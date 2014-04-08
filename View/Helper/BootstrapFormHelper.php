d08cb732b49daeeb4863df1a05bc0f3acf2d2763
Daniel Golgher <daniel@2km.com.br>
2014-04-08 11:42:35 -0300

    add field-mask option

--- View/Helper/BootstrapFormHelper.php
+++ View/Helper/BootstrapFormHelper.php
@@ -63,47 +63,60 @@ class BootstrapFormHelper extends FormHelper {
 		return $this->_inputDefaults;
 	}
 	public function input($fieldName, $options = array()) {
+		$this->setEntity($fieldName);
+		$field_array = $this->entity();
+		$currentModel = $this->_getModel($field_array[0]);
+		$field = array_pop($field_array);
 		//put placeholders
-		if(isset($this->_models[$this->defaultModel]->placeholder) && !isset($options['placeholder'])){
-			$text = $this->_getOnlyField($fieldName);
-			if(isset($this->_models[$this->defaultModel]->placeholder[$text])){
-				$options['placeholder']=$this->_models[$this->defaultModel]->placeholder[$text];
+		if(isset($currentModel->placeholder) && !isset($options['placeholder'])){
+			if(isset($currentModel->placeholder[$field])){
+				$options['placeholder']=$currentModel->placeholder[$field];
 			}
 		}
 		//put tooltips
-		if(isset($this->_models[$this->defaultModel]->tooltips)){
-			$text = $this->_getOnlyField($fieldName);
-			if(isset($this->_models[$this->defaultModel]->tooltips[$text])){
+		if(isset($currentModel->tooltips)){
+			if(isset($currentModel->tooltips[$field])){
 				if(isset($options['label']) && $options['label']===false){
 					$options['label']=false;
 				}
 				else if(!empty($options['label'])){
-					//$options['label']= $options['label'].' <span data-tooltip class="has-tip tip-top" data-width="250" title="'.$this->_models[$this->defaultModel]->tooltips[$text].'">'.$this->Html->image('/DkmUI/img/icon_interrogation.png').'</span>';
-					//$options['label']= $options['label'].' <button type="button" class="btn btn-xs btn-default cakeui-tooltip" data-toggle="tooltip" data-placement="right" title="'.$this->_models[$this->defaultModel]->tooltips[$text].'">?</button>';
-					$options['label']= $options['label'].'<span class="btn btn-xs btn-link cakeui-tooltip" data-toggle="tooltip" data-placement="right" title="'.$this->_models[$this->defaultModel]->tooltips[$text].'">?</button>';
+					$options['label']= $options['label'].'<span class="btn btn-xs btn-link cakeui-tooltip" data-toggle="tooltip" data-placement="right" title="'.$currentModel->tooltips[$field].'">?</button>';
 				} else{
 					$label = __(Inflector::humanize(Inflector::underscore($fieldName)));
-					//$options['label']=$label.' <span data-tooltip class="has-tip tip-top noradius" data-width="210" title="'.$this->_models[$this->defaultModel]->tooltips[$text].'">'.$this->Html->image('/DkmUI/img/icon_interrogation.png').'</span>';
-					$options['label']= $label.'<button type="button" class="btn btn-xs btn-link cakeui-tooltip" data-toggle="tooltip" data-placement="right" title="'.$this->_models[$this->defaultModel]->tooltips[$text].'">?</button>';
+					$options['label']= $label.'<button type="button" class="btn btn-xs btn-link cakeui-tooltip" data-toggle="tooltip" data-placement="right" title="'.$currentModel->tooltips[$field].'">?</button>';
+				}
+			}
+		}
+		//put mask
+		if(isset($this->_models[$this->defaultModel]->mask) && !isset($mask['data-inputmask'])){
+			if(isset($this->_models[$this->defaultModel]->mask[$field])){
+				if(is_array($this->_models[$this->defaultModel]->mask[$field])){
+					if(!isset($options['class'])){$options['class']=null;}
+					$options['class'].='form-control '.$this->_models[$this->defaultModel]->mask[$field]['class'];
+				}
+				else{
+					$options['data-inputmask']=$this->_models[$this->defaultModel]->mask[$field];
 				}
 			}
 		}
 		if($this->isFieldError($fieldName)){
-			$options['div']['class']='has-error has-feedback';
-			$options['after']='<span class="glyphicon glyphicon-remove form-control-feedback"></span>';
+			if(!isset($options['div']['class'])){$options['div']['class']=null;}
+			if(!isset($options['after'])){$options['after']=null;}
+			$options['div']['class'].=' has-error has-feedback';
+			$options['after'].='<span class="glyphicon glyphicon-remove form-control-feedback"></span>';
 		}
 		
 		return parent::input($fieldName, $options);
 	}
-	private function _getOnlyField($fieldName){
-		if (strpos($fieldName, '.') !== false) {
-			$fieldElements = explode('.', $fieldName);
-			$text = array_pop($fieldElements);
-		} else {
-			$text = $fieldName;
-		}
-		return $text;
-	}
+	// private function _getFieldName($fieldName){
+	// 	if (strpos($fieldName, '.') !== false) {
+	// 		$fieldElements = explode('.', $fieldName);
+	// 		$text = array_pop($fieldElements);
+	// 	} else {
+	// 		$text = $fieldName;
+	// 	}
+	// 	return $text;
+	// }
 	public function inlineForm($model,$fieldName,$buttonName,$options){
 		if(!isset($options['Button'])){$options['Button']=null;}
 		if(!isset($options['Field'])){$options['Field']=null;}
